4bf9b76ccc7551a7d47d2fb5edcb5118fac8690f
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-17 12:52:14 -0300

    Update Main

--- uri/src/uri1001/Main.java
+++ uri/src/uri1001/Main.java
@@ -10,13 +10,14 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int A = sc.nextInt();
-		int B = sc.nextInt();
-		int C = sc.nextInt();
-		int D = sc.nextInt();
-		int DIFERENCA = (A * B - C *D);
+		int funcionario = sc.nextInt();
+		int horasTrabalhadas = sc.nextInt();
+		double valorHora = sc.nextDouble();
+		double salario = horasTrabalhadas * valorHora;
 		
-		System.out.println("DIFERENCA = " + DIFERENCA);
+		
+		System.out.println("NUMBER = " + funcionario);
+		System.out.printf("SALARY = U$ %.2f%n", salario);
 		sc.close();
 	}
 
