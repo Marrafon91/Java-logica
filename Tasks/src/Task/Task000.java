6b9d37a11b5196dc6efbcee3e14eaa86f7cceeeb
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-17 09:34:28 -0300

    Summary

---
+++ Tasks/src/Task/Task000.java
@@ -0,0 +1,10 @@
+package Task;
+
+public class Task000 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- logica_programacao/src/logica_programacao/Tipos_String.java
+++ logica_programacao/src/logica_programacao/Tipos_String.java
@@ -12,7 +12,7 @@ public class Tipos_String {
 		int x;
 		String s1, s2, s3;
 		x = sc.nextInt();
-		sc.nextLine();
+		sc.nextLine(); // para nao consumir uma linha
 		s1 = sc.nextLine();
 		s2 = sc.nextLine();
 		s3 = sc.nextLine();
