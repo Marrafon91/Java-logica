c59e33f285d96d141870e12261d83fd70b4a38ce
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-17 08:39:18 -0300

    Create Casting

---
+++ logica_programacao/src/logica_programacao/Casting.java
@@ -0,0 +1,28 @@
+package logica_programacao;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Casting {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b;
+		double resultado;
+		
+		a = 5;
+		b = 2;
+		
+		resultado = (double) a / b;
+		
+		System.out.println(resultado);
+		
+		
+		
+		sc.close();
+
+	}
+
+}
