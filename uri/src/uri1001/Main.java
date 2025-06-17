234717065a64579cbf4794071a36f9e5e84bdb29
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-17 18:20:31 -0300

    Update Main

--- uri/src/uri1001/Main.java
+++ uri/src/uri1001/Main.java
@@ -10,11 +10,22 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		double pi =3.14159;
-		double R = sc.nextDouble();
-		double raio = (4/3.0) * pi * R * R * R;
+		double A = sc.nextDouble();
+		double B = sc.nextDouble();
+		double C = sc.nextDouble();
 		
-		System.out.printf("VOLUME = %.3f%n", raio);
+		double triangulo = (A * C) / 2.0;
+		double circulo = 3.14159 * C * C;
+		double trapezio = (A + B) / 2.0 * C;
+		double quadrado = B * B;
+		double retangulo = A * B;
+		
+		System.out.printf("TRIANGULO: %.3f%n", triangulo);
+		System.out.printf("CIRCULO: %.3f%n", circulo);
+		System.out.printf("TRAPEZIO: %.3f%n", trapezio);
+		System.out.printf("QUADRADO: %.3f%n", quadrado);
+		System.out.printf("RETANGULO: %.3f%n", retangulo);
+		
 		
 		sc.close();
 	}
