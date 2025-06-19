8ca6e6550405b16b372ae017d628b71a7c62ed39
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-19 15:58:48 -0300

    idade valida

--- logica_programacao/src/logica_programacao/Idade_valida.java
+++ logica_programacao/src/logica_programacao/Idade_valida.java
@@ -1,10 +1,31 @@
 package logica_programacao;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Idade_valida {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int idade = sc.nextInt();
+		int soma = 0;
+		int cont = 0;
+		
+		while(idade >= 0) {
+			soma += idade;	
+			cont += 1;
+			idade = sc.nextInt();
+						
+			}
+			if(cont > 0) {
+			double media = (double) soma / cont;
+			System.out.printf("Media da idade = %.2f%n", media);
+		}	else {
+			System.out.println("Impossivel calcular.");
+		}
+		sc.close();
 	}
 
 }
