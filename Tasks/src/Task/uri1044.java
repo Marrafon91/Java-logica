d9138a4cf5bf5737590997af6d634ce657be0d5e
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-19 17:45:43 -0300

    Summary

--- Tasks/src/Task/uri1044.java
+++ Tasks/src/Task/uri1044.java
@@ -1,10 +1,24 @@
 package Task;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class uri1044 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		if(A % B == 0 || B % A == 0) {
+			System.out.println("Sao Multiplos");
+		} else {
+			System.out.println("Nao sao Multiplos");
+		}
+		
+		sc.close();
 	}
 
 }
