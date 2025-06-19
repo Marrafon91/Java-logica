489e7c561bcef50738cea10bb70a43e1870703cc
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-19 13:50:31 -0300

    uri1042

--- Tasks/src/Task/uri1042.java
+++ Tasks/src/Task/uri1042.java
@@ -1,48 +1,47 @@
-package uri1001;
+package Task;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class uri1042 {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-
+		
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
-
+		
 		int menor, meio, maior;
-		if (a <= b && a <= c) {
+		if(a <= b && a <= c) {
 			menor = a;
-		} else if (b <= a && b <= c) {
+		} else if(b <= a && b <= c) {
 			menor = b;
 		} else {
 			menor = c;
 		}
-
-		if (a >= b && a >= c) {
+				
+		if(a >= b && a >= c) {
 			maior = a;
-		} else if (b >= a && b >= c) {
+		} else if(b >= a && b >= c) {
 			maior = b;
 		} else {
 			maior = c;
 		}
-
+		
 		meio = a + b + c - menor - maior;
-
+		
 		System.out.println(menor);
-		System.out.println(meio);
-		System.out.println(maior);
-		System.out.println();
-		System.out.println(a);
-		System.out.println(b);
-		System.out.println(c);
+        System.out.println(meio);
+        System.out.println(maior);        
+        System.out.println();         
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+        sc.close();		
 
-		sc.close();
-
-		sc.close();
 	}
 
 }
