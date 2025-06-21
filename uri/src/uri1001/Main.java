9f196db82a84b02d2ac69149c5ae8bdd350dd4e8
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-21 20:49:30 -0300

    Update

--- uri/src/uri1001/Main.java
+++ uri/src/uri1001/Main.java
@@ -9,18 +9,13 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-
 		int x = sc.nextInt();
 
 		for (int i = 1; i <= x; i++) {
 			if (i % 2 != 0) {
 				System.out.println(i);
 			}
-
 		}
-
 		sc.close();
 	}
 }
