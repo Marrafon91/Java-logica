8fdf0b5da3eda9febe1702daac7ced1267425373
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-21 11:44:37 -0300

    Summary

--- Tasks/src/Task/uri1049.java
+++ Tasks/src/Task/uri1049.java
@@ -1,9 +1,9 @@
-package uri1001;
+package Task;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class uri1049 {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
