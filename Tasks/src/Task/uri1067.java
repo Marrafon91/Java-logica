930ef25586ae8532091c60c60aa10ba6a6fe154c
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-21 19:56:57 -0300

    uri1067

--- Tasks/src/Task/uri1067.java
+++ Tasks/src/Task/uri1067.java
@@ -1,14 +1,11 @@
-package uri1001;
+package Task;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class uri1067 {
 
 	public static void main(String[] args) {
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner(System.in);
-
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
