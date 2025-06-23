3bceabbe15bd7b32e21e57e1c511fb639c059303
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-23 10:38:59 -0300

    Matriz001

---
+++ logica_programacao/src/logica_programacao/Matriz001.java
@@ -0,0 +1,31 @@
+package logica_programacao;
+
+import java.util.Scanner;
+
+public class Matriz001 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int M = sc.nextInt();
+		int N = sc.nextInt();
+
+		int[][] mat = new int[M][N];
+
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+
+		for (int i = 0; i < M; i++) {
+			for (int j = 0; j < N; j++) {
+				System.out.print(mat[i][j] + " ");
+			}
+			System.out.println();
+		}
+
+		sc.close();
+	}
+
+}
