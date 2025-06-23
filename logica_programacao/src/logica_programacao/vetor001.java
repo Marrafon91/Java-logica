2fc7583979e976babc373adade8ba6ddb26d0862
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-23 09:07:13 -0300

    vetor002

--- logica_programacao/src/logica_programacao/vetor001.java
+++ logica_programacao/src/logica_programacao/vetor001.java
@@ -3,14 +3,13 @@ package logica_programacao;
 import java.util.Locale;
 import java.util.Scanner;
 
-public class vetor01 {
+public class vetor001 {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		int N = sc.nextInt();
-		
+		int N = sc.nextInt();		
 		int[] vetor = new int[N];
 		
 		for(int i=0;i<N;i++) {
--- logica_programacao/src/logica_programacao/vetor002.java
+++ logica_programacao/src/logica_programacao/vetor002.java
@@ -1,10 +1,32 @@
 package logica_programacao;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class vetor002 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
+		int N = sc.nextInt();
+		double[] vet = new double[N];
+
+		for (int i = 0; i < N; i++) {
+			vet[i] = sc.nextDouble();
+			System.out.print(vet[i] + " ");
+		}
+		System.out.println();
+
+		double soma = 0;
+		for (int i = 0; i < N; i++) {
+			soma = soma + vet[i];
+		}
+		System.out.printf("%.2f%n", soma);
+
+		double media = soma / N;
+		System.out.printf("%.2f%n", media);
+
+		sc.close();
 	}
-
 }
