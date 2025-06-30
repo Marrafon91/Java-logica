d46a0d9248a1bda6b244da8f2c810aca3e5e615f
GuiM <Guilherme.Marrafon1@gmail.com>
2025-06-30 20:02:55 -0300

    Up

--- uri/src/uri1001/Main.java
+++ uri/src/uri1001/Main.java
@@ -9,18 +9,8 @@ public class Main {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int N = sc.nextInt();
-		for (int i = 0; i < N; i++) {
-			int X = sc.nextInt();
-			int Y = sc.nextInt();
-
-			if (Y == 0) {
-				System.out.println("divisao impossivel");
-			} else {
-				double resultado = (double) X / Y;
-				System.out.printf("%.1f%n", resultado);
-			}
-		}
-		sc.close();
+		
+		sc.close(); 
+		
 	}
 }
