7fc20f01b66897dc5b75c6a442f9df6ee22350de
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-17 20:45:04 -0300

    Update

--- uri/src/TasksUri/Main.java
+++ uri/src/TasksUri/Main.java
@@ -12,10 +12,12 @@ public class Main {
 		int n = sc.nextInt();
 
 		int contador = 1;
+
 		for (int i = 1; i <= n; i++) {
 			int contador1 = contador * contador;
 			int contador2 = contador * contador * contador;
 			System.out.printf("%d %d %d\n", contador, contador1, contador2);
+			System.out.printf("%d %d %d\n", contador, contador1 + 1, contador2 + 1);
 			contador += 1;
 		}
 		sc.close();
