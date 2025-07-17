6dc7bc47c6f52582fbd19be563ba1226cb65ca2c
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-17 07:24:16 -0300

    Summary

--- Tasks/src/Task/uri1132.java
+++ Tasks/src/Task/uri1132.java
@@ -1,9 +1,9 @@
-package uri1001;
+package Task;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class uri1132 {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
@@ -11,9 +11,17 @@ public class Main {
 
 		int x = sc.nextInt();
 		int y = sc.nextInt();
+		
+		int menor = x;
+		int maior = y;
+		
+		if(x > y) {
+			menor = y;
+			maior = x;
+		}
 
 		int soma = 0;
-		for (int i = x; i <= y; i++) {
+		for (int i = menor; i <= maior; i++) {
 			if (i % 13 != 0) {
 				soma += i;
 			}
