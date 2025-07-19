f8b725155c37a9ce7aef969979443cd632b49af7
GuiM <Guilherme.Marrafon1@gmail.com>
2025-07-18 21:09:51 -0300

    Summary

--- Tasks/src/Task/uri1153.java
+++ Tasks/src/Task/uri1153.java
@@ -1,22 +1,22 @@
-package TasksUri;
+package Task;
 
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Main {
+public class uri1153 {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 
-		int n = sc.nextInt();
-
+		int n = sc.nextInt();		
+		
 		int fat = 1;
-		for (int i = 1; i <= n; i++) {
-			fat *= i;
+		for(int i = 1; i <= n; i++) {
+			fat *= i;			
 		}
 		System.out.println(fat);
-
+		
 		sc.close();
 	}
 }
