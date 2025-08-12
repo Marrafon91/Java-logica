2f2eff7564d96dc65642c475d4957ec56f4c344c
Guilherme Marrafon <Guilherme.Marrafon1@gmail.com>
2025-08-12 16:31:14 -0300

    Att

--- logica_programacao/src/logica_programacao/do_while.java
+++ logica_programacao/src/logica_programacao/do_while.java
@@ -12,11 +12,11 @@ public class do_while {
 		char resp;
 		
 		do {
-			System.out.println("Digite a temperatura em Celsius: ");
+			System.out.print("Digite a temperatura em Celsius: ");
 			double C = sc.nextDouble();
 			double F = 9.0 * C / 5.0 + 32.0;
 			System.out.printf("Equivalente em Fahrenheit: %.1f%n", F);
-			System.out.println("Deseja repetir (s/n)? ");
+			System.out.print("Deseja repetir (s/n)? ");
 			 resp = sc.next().charAt(0);
 		} while (resp != 'n');		
 		
